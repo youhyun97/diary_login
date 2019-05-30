9ed2e16017042c263520e42042016897f75de4ea
Kim eunji <emliy112@naver.com>
2019-05-31 04:10:11 +0900

    mypage layout 수정

--- app/src/main/java/com/androidtutorialshub/loginregister/activities/FifthActivity.java
+++ app/src/main/java/com/androidtutorialshub/loginregister/activities/FifthActivity.java
@@ -1,4 +1,46 @@
 package com.androidtutorialshub.loginregister.activities;
 
-public class FifthActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidtutorialshub.loginregister.R;
+import com.androidtutorialshub.loginregister.adapters.UsersRecyclerAdapter;
+import com.androidtutorialshub.loginregister.sql.DatabaseHelper;
+
+public class FifthActivity extends AppCompatActivity {
+    private UsersRecyclerAdapter usersRecyclerAdapter;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fifth);
+
+        TextView email2 = (TextView) findViewById(R.id.email_2);
+        TextView pw2 = (TextView) findViewById(R.id.pw_2);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu); // 메뉴 xml파일 등록
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent menuIntent1 = new Intent(FifthActivity.this, FifthActivity.class);
+        startActivity(menuIntent1);
+        finish();
+
+        return true;
+    }
 }
